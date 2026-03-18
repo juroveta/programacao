dac7e9326108781ef6bad4030826bab7b5d9d033
julia roveta <juliarove06@gmail.com>
2026-03-18 19:45:39 -0300

    terminei o codigo

--- atividade3.cpp
+++ atividade3.cpp
@@ -11,12 +11,11 @@ private:
     double valorAtual;
 
 public:
-    SensorNivel (String novaTag)
-      ()
-    // TODO: criar um construtor minimo que receba apenas a tag.
-    // Dica: use uma faixa padrao coerente.
-
-    // TODO: criar um construtor completo com tag, minimo, maximo e valorAtual.
+    SensorNivel (string novaTag)
+     : tag (novaTag), minimo (0.0), maximo (100.0), valorAtual (0.0) {}
+    
+    SensorNivel (string novaTag, double novoMinimo, double novoMaximo, double novoValor)
+     : tag (novaTag), minimo (novoMinimo), maximo (novoMaximo), valorAtual (novoValor) {}
 
     void exibirResumo() const {
         cout << "[SensorNivel] " << tag
@@ -32,10 +31,12 @@ private:
     double velocidade;
 
 public:
-    // TODO: criar um construtor minimo.
+    Bomba (string novatag)
+    : tag(novatag) , velocidade(0.0) {}
 
-    // TODO: criar um construtor completo com velocidade inicial.
-
+    Bomba (string novatag, double novavelocidade)
+    : tag(novatag) , velocidade(novavelocidade) {}
+    
     void exibirResumo() const {
         cout << "[Bomba] " << tag
              << " | velocidade: " << velocidade << "%\n";
@@ -49,9 +50,11 @@ private:
     double kp;
 
 public:
-    // TODO: criar um construtor minimo.
+    ControladorTanque (string novatag)
+    : tag(novatag), setpoint (0.0), kp(0.0){}
 
-    // TODO: criar um construtor completo com setpoint e kp.
+    ControladorTanque (string novatag, double novosetpoint, double novokp)
+    : tag(novatag), setpoint (novosetpoint), kp(novokp){}
 
     void exibirResumo() const {
         cout << "[ControladorTanque] " << tag
@@ -61,12 +64,21 @@ public:
 };
 
 int main() {
-    // TODO: crie ao menos dois objetos de cada classe usando sobrecarga.
-    // Exemplo esperado:
-    // SensorNivel sn1("LT-401");
-    // SensorNivel sn2("LT-402", 0.0, 5000.0, 2780.0);
+     SensorNivel sn1("LT-401");
+    SensorNivel sn2("LT-402", 0.0, 5000.0, 2780.0);
 
-    // TODO: chame exibirResumo() para todos os objetos.
+    Bomba b1("P-101");
+    Bomba b2("P-102", 65.0);
+
+    ControladorTanque c1("LIC-401");
+    ControladorTanque c2("LIC-402", 3200.0, 1.8);
+
+    sn1.exibirResumo();
+    sn2.exibirResumo();
+    b1.exibirResumo();
+    b2.exibirResumo();
+    c1.exibirResumo();
+    c2.exibirResumo();
 
     return 0;
 }
